dc6fd0ac7bb6f4b2c8f6ac4de7983003729c54b0
DanielaOrd97 <201G0272@rcarbonifera.tecnm.mx>
2024-05-22 00:31:39 -0600

    ...

--- PEPELTORO/Hubs/GatoHubs/GatoHub.cs
+++ PEPELTORO/Hubs/GatoHubs/GatoHub.cs
@@ -4,26 +4,47 @@ namespace PEPELTORO.Hubs.GatoHubs
 {
     public class GatoHub:Hub
     {
-        public static Dictionary<string, string> users= new Dictionary<string, string>();
+        public static Dictionary<string, string> users = new Dictionary<string, string>();
 
         public async Task IniciarSesion(string nombreUsuario)
         {
-            if (users.Values.Any(
-                x=>x.Equals(nombreUsuario, StringComparison.OrdinalIgnoreCase)
+            if (users.Keys.Any(
+                x => x.Equals(nombreUsuario, StringComparison.OrdinalIgnoreCase)
                ))
             {
                 await Clients.Caller.SendAsync("Receivemessage", "error", "el nombre de usuario existe");
             }
             else
             {
-                users[Context.ConnectionId]= nombreUsuario;
+                //users[Context.ConnectionId]= nombreUsuario;
+                users[nombreUsuario] = Context.ConnectionId;
                 await Clients.Caller.SendAsync("Receivemessage", "ok", "ya pegaste morro");
             }
         }
         public static Queue<string> cola = new Queue<string>();
-        public static  Task BuscarPartida(string nombreUsuario)
+
+        public static int NumPartida = 0;
+
+        public async Task BuscarPartida(string nombreUsuario)
         {
-            return null;
-        } /////pll
+            if (cola.Count == 0)
+            {
+                cola.Enqueue(nombreUsuario);
+            }
+            else
+            {
+                var contrincante = cola.Dequeue();
+                string partida = $"partida{NumPartida}";
+                await Groups.AddToGroupAsync(Context.ConnectionId, partida);
+                await Groups.AddToGroupAsync(users[contrincante], partida);
+                NumPartida++;
+                await Clients.Groups(partida).SendAsync("Game started", partida);
+
+                //notificar al usuario que es su turno.
+                await Clients.Users(Context.ConnectionId).SendAsync("Play");
+
+            }
+
+        }
     }
 }
