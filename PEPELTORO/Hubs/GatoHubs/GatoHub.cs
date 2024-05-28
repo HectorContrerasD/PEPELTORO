779f33d9e44d3994fb8afdb442feaf5e7a68a6e7
DanielaOrd97 <201G0272@rcarbonifera.tecnm.mx>
2024-05-28 12:51:55 -0600

    ..

--- PEPELTORO/Hubs/GatoHubs/GatoHub.cs
+++ PEPELTORO/Hubs/GatoHubs/GatoHub.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using PEPELTORO.Models;
 
 namespace PEPELTORO.Hubs.GatoHubs
 {
     public class GatoHub:Hub
     {
         public static Dictionary<string, string> users = new Dictionary<string, string>();
+        public static Dictionary<string, Partida> partidas = new Dictionary<string, Partida>();
 
         public async Task IniciarSesion(string nombreUsuario)
         {
@@ -42,9 +44,53 @@ namespace PEPELTORO.Hubs.GatoHubs
 
                 //notificar al usuario que es su turno.
                 await Clients.Users(Context.ConnectionId).SendAsync("Play");
+                var datospartida = new Partida()
+                {
+                    NombrePartida = partida,
+                    NombreUsuario1 = nombreUsuario,
+                    ConnectionId1 = Context.ConnectionId,
+                    NombreUsuario2 = contrincante,
+                    ConnectionId2 = users[contrincante],
+                    Turno = 'X'
+                };
+
+                partidas[partida] = datospartida;
 
             }
 
         }
+
+
+        public async Task Jugar(string partida, string nombreUsuario, string tablero)
+        {
+            var datosPartida = partidas[partida];
+            if (GanaXO(datosPartida.Turno, tablero))
+            {
+                //Se notifica que gano
+                await Clients.Group(partida).SendAsync("GameOver", nombreUsuario);
+            }
+            else
+            {
+                datosPartida.Turno = (datosPartida.Turno == 'X' ? 'O' : 'X');
+                await Clients.Group(partida).SendAsync(""); //pendiente
+            }
+        }
+
+        private bool GanaXO(char turno, string tablero)
+        {
+            return false;
+        }
+
+        int[,] lineas = new int[,]
+        {
+            {0,1,2 },   //...........
+            {3,4,5 },
+            {6,7,8 },
+            {0,3,6 },
+            {1,4,7 },
+            {2,5,8 }
+        };
+
+        //AVANZAR A CLIENTE.
     }
 }
